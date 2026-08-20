48edb0409d7735c930c9d0ab58335102f0896110
moussa04gueye <mousse04gueye@gmail.com>
2026-08-20 14:03:18 +0000

    Update import path for User model in auth.service.ts

--- frontend/src/app/core/services/auth.service.ts
+++ frontend/src/app/core/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import { User } from '../models/User.model';
+import { User } from '../models/User';
 
 const TOKEN_KEY = 'admin_token';
 
