4e6ead82ed2fd041f7c50bb4bb66cc99bf95a28f
moussa04gueye <mousse04gueye2gmail.com>
2026-08-21 11:09:59 +0000

    the last seetings

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,8 +2,4 @@ export const environment = {
   production: false,
   apiUrl: 'https://my-portfolio-l2dk.onrender.com/api',
   sanctumUrl: 'https://my-portfolio-l2dk.onrender.com',
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> 64a97eb (the last seetings)
